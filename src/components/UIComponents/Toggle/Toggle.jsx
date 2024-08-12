7855eb771a8a419a36290fe8ca8ca8e4e97696e4
donskojkowboj <147949437+donskojkowboj@users.noreply.github.com>
2024-08-12 15:42:40 +0300

    убран лишний див и его стили

--- src/components/UIComponents/Toggle/Toggle.jsx
+++ src/components/UIComponents/Toggle/Toggle.jsx
@@ -2,20 +2,18 @@ import styles from './Toggle.module.scss';
 
 export const Toggle = ({ checked, onChange, title }) => {
   return (
-    <div className={styles.wrapper}>
-      <label className={styles.label} htmlFor={title}>
-        <input
-          className={styles.toggle}
-          id={title}
-          checked={checked}
-          onChange={onChange}
-          type="checkbox"
-        />
-        <span className={styles.toggleBackground}>
-          <span className={styles.toggleButton} />
-        </span>
-        <span className={styles.text}>{title}</span>
-      </label>
-    </div>
+    <label className={styles.label} htmlFor={title}>
+      <input
+        className={styles.toggle}
+        id={title}
+        checked={checked}
+        onChange={onChange}
+        type="checkbox"
+      />
+      <span className={styles.toggleBackground}>
+        <span className={styles.toggleButton} />
+      </span>
+      <span className={styles.text}>{title}</span>
+    </label>
   );
 };
